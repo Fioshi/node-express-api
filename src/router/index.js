bef8c06137278ae4f9f82dba816f281137b72a8b
Fioshi <guilhermefiochi@outlook.com>
2024-08-15 21:44:49 -0300

    Adicionado controle por token

--- src/router/index.js
+++ src/router/index.js
@@ -17,4 +17,29 @@ router.get('/privada', (req, res) => {
     return res.send('Acessado').status(200);
 })
 
+const tokenExemplos = {
+    'tokenAdmin': {role: 'admin'},
+    'tokenUser': {role:'user'},
+    'tokenConvidado': {role: 'guest'}
+}
+
+router.get('/admin', (req, res)=> {
+    const token = req.headers['authorization'];
+
+    if(!token) {
+        res.status(401).send("Sem autorização");
+    }
+
+    const user = tokenExemplos[token];
+    if(!user){
+        res.status(401).send("Sem autorização");
+    }
+
+    if(user.role != 'admin'){
+        return res.status(403).send('Não tem permissão para acessar aqui');
+    }
+
+    return res.send('acesso liberado').status(200);
+})
+
 module.exports = router; 
